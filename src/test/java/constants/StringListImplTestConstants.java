6f36b37a23c67afb24efc22a2523937be1bd2830
qmargo23 <elen.asplund@gmail.com>
2023-08-31 15:11:54 +0300

    реализация ArrayList с тестами

---
+++ src/test/java/constants/StringListImplTestConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class StringListImplTestConstants {
+    public static final int SIZE = 4;
+    public static final int STORAGE = 6;
+    public static final Integer INDEX1 = 0;
+    public static final Integer INDEX2 = 12;
+    public static final String ITEM1 = "TEST";
+    public static final String ITEM2 = null;
+//    public static final String ITEM3 = "0";
+}
